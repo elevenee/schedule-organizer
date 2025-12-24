c8744156bda811e080409e2be4f71cff340fb747
elevenee <39453790+elevenee@users.noreply.github.com>
2025-12-24 15:07:48 +0800

    update

--- features/mata-kuliah/service/mata-kuliah.ts
+++ features/mata-kuliah/service/mata-kuliah.ts
@@ -5,11 +5,12 @@ import { Jenjang } from "@prisma/client";
 export async function upsertMataKuliah(data: any) {
     return prisma.mataKuliah.upsert({
         where: {
-            kode_jurusanId_semester_jenjang: {
+            kode_jurusanId_semester_jenjang_kurikulumId: {
                 kode: data.kode,
                 jurusanId: data.jurusanId,
                 semester: data.semester,
                 jenjang: data.jenjang as Jenjang,
+                kurikulumId: data.kurikulumId
             }
         },
         update: data,
