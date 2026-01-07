04365370c31cf1c83200b60fbfa6491ccf69ccfa
elevenee <39453790+elevenee@users.noreply.github.com>
2026-01-07 16:54:53 +0800

    update

--- features/dosen/actions/create.ts
+++ features/dosen/actions/create.ts
@@ -1,14 +1,17 @@
 'use server';
 
 import { prisma } from "@/lib/prisma";
+import { TypeDosen } from "@prisma/client";
 import { dosenFormValues } from "../validations";
 
 export async function create(formData: dosenFormValues) {
-  const { nama, nidn, fakultasId } = formData;
+  const { nama, nidn, fakultasId, jurusanId, status } = formData;
   const data = {
     nama,
     nidn,
-    fakultasId: Number(fakultasId),
+    fakultasId: fakultasId ? Number(fakultasId) : null,
+    jurusanId: jurusanId ? Number(jurusanId) : null,
+    status: status as TypeDosen
   }
   return await prisma.dosen.create({
     data: data
--- features/dosen/actions/update.ts
+++ features/dosen/actions/update.ts
@@ -1,17 +1,19 @@
 'use server';
 
 import { prisma } from "@/lib/prisma";
+import { TypeDosen } from "@prisma/client";
 import { dosenFormValues } from "../validations";
 export async function update(id: number, formData: dosenFormValues) {
     const find = await prisma.dosen.findUnique({ where: { id } });
     if (!find) return { error: 'Dosen tidak ditemukan' };
 
-    const { nama, nidn, fakultasId, jurusanId } = formData;
+    const { nama, nidn, fakultasId, jurusanId, status } = formData;
     const updateData = {
         nama,
         nidn,
         fakultasId: Number(fakultasId),
         jurusanId: jurusanId ? Number(jurusanId) : null,
+        status: status as TypeDosen
     }
 
     const updated = await prisma.dosen.update({
