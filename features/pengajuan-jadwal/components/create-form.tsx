2601a8efb27fbf3d7a20789c7e164815d9b91172
elevenee <39453790+elevenee@users.noreply.github.com>
2026-06-25 10:29:13 +0800

    update tampilan semester

--- features/pengajuan-jadwal/components/create-form.tsx
+++ features/pengajuan-jadwal/components/create-form.tsx
@@ -141,19 +141,56 @@ export function JadwalForm({ form, onSubmit, jenisDosen }: Props) {
 
     const { data: tahunAkademik } = useGetTahunAkademikAktif();
 
-    const SEMESTER = [1, 2, 3, 4, 5, 6, 7, 8, 99];
+    const SEMESTER = [
+        {
+            label: "Semester 1",
+            value: 1
+        },
+        {
+            label: "Semester 2",
+            value: 2
+        },
+        {
+            label: "Semester 3",
+            value: 3
+        },
+        {
+            label: "Semester 4",
+            value: 4
+        },
+        {
+            label: "Semester 5",
+            value: 5
+        },
+        {
+            label: "Semester 6",
+            value: 6
+        },
+        {
+            label: "Semester 7",
+            value: 7
+        },
+        {
+            label: "Semester 8",
+            value: 8
+        },
+        {
+            label: "Semester >",
+            value: 99
+        },
+    ];
 
     useEffect(() => {
         if (tahunAkademik) {
             if (tahunAkademik?.semester === 'GENAP') {
-                setAvailableSemester(SEMESTER.filter((s) => s % 2 === 0).map((semester) => ({
-                    value: semester.toString(),
-                    label: `Semester ${semester}`,
+                setAvailableSemester(SEMESTER.filter((s) => s.value % 2 === 0).map((semester) => ({
+                    value: semester.value.toString(),
+                    label: semester.label,
                 })));
             } else {
-                setAvailableSemester(SEMESTER.filter((s) => s % 2 !== 0).map((semester) => ({
-                    value: semester.toString(),
-                    label: `Semester ${semester}`,
+                setAvailableSemester(SEMESTER.filter((s) => s.value % 2 !== 0).map((semester) => ({
+                    value: semester.value.toString(),
+                    label: semester.label,
                 })));
             }
         }
